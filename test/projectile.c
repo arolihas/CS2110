784486713968cf2cab0d10789d2aad99c824409f
Sahil Arora <36029051+arolihas@users.noreply.github.com>
2018-02-01 02:47:27 -0500

    Add files via upload
    BrickBreaker inspired VBA Gameboy game

---
+++ test/projectile.c
@@ -0,0 +1,20 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=5x5 projectile projectile.png 
+ * Time-stamp: Monday 11/13/2017, 18:29:02
+ * 
+ * Image Information
+ * -----------------
+ * projectile.png 5@5
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#include "projectile.h"
+
+const unsigned short projectile[25] =
+{
+	0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,
+	0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab,0x17ab
+};
+
